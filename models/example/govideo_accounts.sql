c3633eff20e9dcc74c63edbe517efe1bc989942b
anthonytxie <anthony.xie@vidyard.com>
2018-02-16 16:51:14 -0500

    modified govideo_accounts query

--- models/example/govideo_accounts.sql
+++ models/example/govideo_accounts.sql
@@ -44,25 +44,64 @@ with
       and o.stage_name = '7 - Closed Won'
       and p.product_code = 'ENG-101'
   )
-, t2 as (
+  , t2 as (
+    select
+      account_name
+      , account_id
+      , vidyard_account_id parent_id
+      , max(contract_start_date) contract_start_date
+      , max(contract_end_date) contract_end_date
+      , max(renewal_date) renewal_date
+      , max(account_owner) account_owner
+      , max(csm_owner) csm_owner
+      , max(launch_owner) launch_owner
+      , sum(number_of_seats) seats_purchased
+    from
+      t1
+    group by
+      1
+      , 2
+      , 3
+  )
+  , t3 as (
+    select
+      t2.account_id
+      , min(o.contract__start__date___c) first_contract_start_date
+    from
+      salesforce._account a
+      join t2 on
+        t2.account_id = a.id
+      join salesforce._opportunity o on
+        o.account_id = a.id
+      join salesforce._opportunity_line_item ol on
+        o.id = ol.opportunity_id
+      join salesforce._product_2 p on
+        p.id = ol.product_2_id
+    where
+      o.is_deleted is false
+      and a.is_deleted is false
+      and ol.is_deleted is false
+      and o.stage_name = '7 - Closed Won'
+      and p.product_code = 'ENG-101'
+    group by
+      1
+  )
+  
 select
   account_name
-  , account_id
-  , vidyard_account_id parent_id
-  , max(contract_start_date) contract_start_date
-  , max(contract_end_date) contract_end_date
-  , max(renewal_date) renewal_date
-  , max(account_owner) account_owner
-  , max(csm_owner) csm_owner
-  , max(launch_owner) launch_owner
-  , sum(number_of_seats) seats_purchased
+  , t2.account_id
+  , parent_id
+  , contract_start_date
+  , contract_end_date
+  , renewal_date
+  , account_owner
+  , csm_owner
+  , launch_owner
+  , seats_purchased
+  , first_contract_start_date
 from
-  t1
-group by
-  1
-  , 2
-  , 3
-  )
-
+  t2
+  join t3 on
+    t2.account_id = t3.account_id
 select * from t2
 order by 1
